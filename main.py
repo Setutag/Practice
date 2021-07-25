561d09fa9839a277c8d7860bc25134c973739c7f
Setutag <87135809+Setutag@users.noreply.github.com>
2021-07-25 17:17:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,49 +1,105 @@
+import cv2
+import numpy as np
+from PIL import ImageGrab
 from ursina import *
 import math
 
+def analize():
+    signal = 'no signal'
+    screenshot = ImageGrab.grab()
+    rgb_img = np.array(screenshot)
+    bgr_img = cv2.cvtColor(rgb_img, cv2.COLOR_RGB2BGR)
+    hsv_img = cv2.cvtColor(bgr_img, cv2.COLOR_BGR2HSV)
+
+    lb = np.array([30, 70, 70])
+    ub = np.array([120, 255, 120])
+
+    mask = cv2.inRange(hsv_img, lb, ub)
+
+    M = cv2.moments(mask, 1)
+
+    if M['m00'] != 0:
+        x = int(M['m10'] / M['m00'])
+        y = int(M['m01'] / M['m00'])
+
+        cv2.circle(bgr_img, (x, y), 25, (0, 0, 225), -1)
+
+        if x < 810:
+            cv2.putText(bgr_img, 'Left', (40, 100), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 3)
+            signal = 'left'
+        if x > 1110:
+            cv2.putText(bgr_img, 'Right', (40, 100), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 3)
+            signal = 'right'
+        if (x > 810) & (x < 1110):
+            cv2.putText(bgr_img, 'Fine', (40, 100), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
+            signal = 'forward'
+
+    else:
+        signal = 'stop'
+
+    cv2.rectangle(bgr_img, (0, 0), (1920, 1080), (0, 0, 0), 70)
+    cv2.line(bgr_img, (960, 0), (960, 1080), (0, 0, 255), 5)
+    cv2.line(bgr_img, (810, 0), (810, 1080), (0, 0, 255), 5)
+    cv2.line(bgr_img, (1110, 0), (1110, 1080), (0, 0, 255), 5)
+
+    cv2.imshow('Res', cv2.resize(bgr_img, (480, 270)))
+
+    return signal
 class Scene(Entity):
     def __init__(self):
         super().__init__()
         self.model = 'quad'
-        self.texture = 'race_line'
+        self.texture = 'map'
         self.scale_x = 45
         self.scale_y = 45
         self.rotation_x = 90
         self.position = (0, 0, 0)
 
+
 class Player(Entity):
-    def __init__(self, setpos):
+    def __init__(self):
         super().__init__()
-        self.model = 'sphere'
-        self.texture = 'me.png'
-        self.scale = (7, 7, 7)
-        self.rotation_y = -90
-        self.position = (setpos[0], self.scale_y/2, setpos[1])
-        self.angle = math.radians(-90)
-        self.wheel_radius = 0.025
-        self.velocity = [1, 1]
-        self.wheel_between_length = self.scale_z
+        camera.position = [-16.741268157958984, 4, -16.832792282104492]
+        camera.rotation_x = 45
+        camera.rotation_y = 361.1536560058594
+        self.a = 4.732460266782609
+        self.r = 0.025
+        self.v = [1, 1]
+        self.l = 0.5
+        self.k = 30
+        self.s = 1.5
+        self.pic = analize()
 
     def update(self):
-        if held_keys['w']:
-            self.x += 2*(self.wheel_radius / 2) * (self.velocity[0] + self.velocity[1]) * math.cos(self.angle)
-            self.z -= 2*(self.wheel_radius / 2) * (self.velocity[0] + self.velocity[1]) * math.sin(self.angle)
 
-        if held_keys['d']:
-            self.angle += math.radians((self.wheel_radius/self.wheel_between_length)*(151*self.velocity[1] - self.velocity[0]))
-            #self.rotation_y += (self.wheel_radius/self.wheel_between_length)*(151*self.velocity[1] - self.velocity[0])
+        if self.pic == 'forward':
+            camera.x += self.s * (self.r / 2) * (self.v[0] + self.v[1]) * math.cos(self.a)
+            camera.z -= self.s * (self.r / 2) * (self.v[0] + self.v[1]) * math.sin(self.a)
 
-        if held_keys['a']:
-            self.angle += math.radians((self.wheel_radius/self.wheel_between_length)*(self.velocity[1] - 151*self.velocity[0]))
-            #self.rotation_y += (self.wheel_radius/self.wheel_between_length)*(self.velocity[1] - 151*self.velocity[0])
-        self.rotation_y += 2*(self.wheel_radius/self.wheel_between_length)*(self.velocity[1] - 151*self.velocity[0])
+        if self.pic == 'right':
+            self.a += math.radians((self.r / self.l) * (self.k * self.v[1] - self.v[0]))
+            camera.rotation_y += (self.r / self.l) * (self.k * self.v[1] - self.v[0])
+
+        if self.pic == 'left':
+            self.a += math.radians((self.r / self.l) * (self.v[1] - self.k * self.v[0]))
+            camera.rotation_y += (self.r / self.l) * (self.v[1] - self.k * self.v[0])
+
+        if held_keys['r']:
+            self.a = 4.732460266782609
+            camera.x = -16.741268157958984
+            camera.z = -16.832792282104492
+            camera.rotation_y = 361.1536560058594
+
+        if held_keys['t']:
+            print('X: ', camera.x, '\nZ: ', camera.z, '\nAngle:', camera.rotation_y, ' ', self.a )
+
+        self.pic = analize()
+
 
 
 app = Ursina()
-landscape = Scene()
-#audio = Audio('melody.wav', loop = True)
-bot = Player((0, -17))
-camera.position = (0, 30, -100)
-camera.rotation_x = 15
+
+bot = Player()
+land = Scene()
 
 app.run()
